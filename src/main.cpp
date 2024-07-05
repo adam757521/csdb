78d9d296ce869070caced5b0aea3384dc1d189b2
Virtual <nitaystain090@gmail.com>
2024-07-05 14:31:32 +0300

    add dir alignment

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,36 @@
 #include <cstdint>
-#include <serialization.h>
-#include <field.h>
 #include <iostream>
 #include <fstream>
 #include "asio.hpp"
+#include <serialization.h>
+#include <db/field.h>
 
 int main()
 {
     // char a = decode_zigzag<char>(encode_zigzag<char>(-5));
     // std::cout << signed(a) << std::endl;
-    auto buff = encode_zigzag<int>(INT32_MAX);
-    std::cout << decode_zigzag<int>(buff) << std::endl;
-    return 0;
+    // auto buff = encode_zigzag<int>(INT32_MAX);
+    // std::cout << decode_zigzag<int>(buff) << std::endl;
+    // return 0;
 
-    FieldHeader header(FieldOptions::Nullable | FieldOptions::List, FieldType::Text, "Username");
-    std::ofstream file("csdb.db");
+    // FieldHeader header(FieldOptions::Nullable | FieldOptions::List, FieldType::Text, "Username");
+    // std::ofstream file("csdb.db");
 
-    if (!file.is_open())
-    {
-        std::cerr << "cannot open file." << std::endl;
-        return 1;
-    }
-    file << header;
-    file.close(); // to write changes
-    // std::ios_base::app | std::ios_base::in | std::ios_base::out
+    // if (!file.is_open())
+    // {
+    //     std::cerr << "cannot open file." << std::endl;
+    //     return 1;
+    // }
+    // file << header;
+    // file.close(); // to write changes
+    // // std::ios_base::app | std::ios_base::in | std::ios_base::out
 
-    std::fstream input("csdb.db");
-    FieldHeader new_header;
-    input >> new_header;
-    input.close();
+    // std::fstream input("csdb.db");
+    // FieldHeader new_header;
+    // input >> new_header;
+    // input.close();
 
-    std::cout << new_header.GetName() << " type values: " << (int)new_header.GetType() << " " << (int)new_header.GetOptions() << std::endl;
+    // std::cout << new_header.GetName() << " type values: " << (int)new_header.GetType() << " " << (int)new_header.GetOptions() << std::endl;
 
     return 0;
 }
